71691313e15279ffc4db3ddb78a3151fc02ebf96
Silva <gabrielsilvacarvalho@id.uff.br>
2022-10-02 12:16:07 -0300

    Arquivos para exercicios Js/Ts treinamento Ps2022

---
+++ base.ts
@@ -0,0 +1,18 @@
+import people from './people.json'
+
+// https://www.unimed.coop.br/viver-bem/pais-e-filhos/estatura-por-idade
+
+/** Faça a implementação da função alturaMedia, que deve receber um argumento IPeople, e verificar se a pessoa está acima, abaixo ou exatamente na altura média
+ *  OBS: as idades utilizadas serão 5, 10 e 15 apenas.
+ */
+
+interface IPeople  {
+    nome: string,
+    sexo: string,
+    idade: number,
+    altura: number
+}
+
+export function alturaMedia() {
+    //implementação
+}
